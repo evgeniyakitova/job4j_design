3073f9119cd4ee8a9f6f1d0d5ff3ad79513329be
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-12-03 02:42:01 +0300

    Вывод результата построчно

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +24,6 @@ public class LogFilter {
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        log.forEach(System.out::println);
     }
 }
