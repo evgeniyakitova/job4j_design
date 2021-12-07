19173a343e1f0ec8491d10d02776ca1c063fa4b2
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-12-08 01:54:47 +0300

    Изменение поиска дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -4,16 +4,28 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Set<FileProperty> files = new HashSet<>();
+    private final Map<FileProperty, List<Path>> files = new HashMap<>();
+
+    public List<Path> getDuplicates() {
+        return files.values()
+                .stream()
+                .filter(list -> list.size() > 1)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        long size = file.toFile().length();
-        String name = file.toFile().getName();
-        if (!files.add(new FileProperty(size, name))) {
-            System.out.println(file.toAbsolutePath());
+        FileProperty newFile = new FileProperty(attrs.size(), file.toFile().getName());
+        if (files.containsKey(newFile)) {
+           files.get(newFile).add(file.toAbsolutePath());
+        } else {
+            List<Path> listOfPaths = new ArrayList<>();
+            listOfPaths.add(file.toAbsolutePath());
+            files.put(newFile, listOfPaths);
         }
         return FileVisitResult.CONTINUE;
     }
