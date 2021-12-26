2f2371bff69b2c411e381c77a506f59a45cf5d51
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2021-12-26 17:54:38 +0300

    0. Join [#6862 #244366]

---
+++ data/departaments.sql
@@ -0,0 +1,44 @@
+create table departments(
+    id serial primary key,
+    name varchar(255)
+);
+create table employees(
+    id serial primary key,
+    name varchar(255),
+    department_id int references departments(id)
+);
+
+insert into departments(name) values ('Finance'), ('IT'), ('Investigation');
+
+insert into employees(name, department_id) values ('Irina', 1), ('Mariya', 2), ('Petr', 2);
+
+select * from departments dep join employees emp
+on dep.id = emp.department_id;
+
+select * from departments dep left join employees emp
+on dep.id = emp.department_id;
+
+select * from departments dep right join employees emp
+on dep.id = emp.department_id;
+
+select * from employees emp left join departments dep
+on dep.id = emp.department_id;
+
+select * from departments dep full join employees emp
+on dep.id = emp.department_id;
+
+select * from departments dep cross join employees emp;
+
+select dep.name from departments dep left join employees emp
+on dep.id = emp.department_id
+where emp.department_id is null;
+
+create table teens(
+  name varchar(255),
+  gender varchar(10)
+);
+
+insert into teens(name, gender) values ('Irina', 'female'), ('Mariya', 'female'), ('Petr', 'male');
+
+select * from teens t1 cross join teens t2
+where t1.gender != t2.gender;
