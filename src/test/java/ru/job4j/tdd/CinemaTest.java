0211c2eaaa0da94452f491e3c51da091944e70af
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2022-02-15 06:01:00 +0300

    Добавление тестов в CinemaTest

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -39,4 +39,33 @@ public class CinemaTest {
         List<Session> sessions = cinema.find(session -> true);
         assertThat(sessions.size(), is(2));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketForInvalidSeatThenException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.OCTOBER, 10, 23, 0);
+        cinema.buy(account, 30, 5, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketForInvalidDateThenException() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2023, Calendar.OCTOBER, 10, 23, 0);
+        cinema.buy(account, 5, 5, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketAndSeatIsOccupiedThenException() {
+        Account firsAccount = new AccountCinema();
+        Account secondAccount = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.OCTOBER, 10, 23, 0);
+        cinema.buy(firsAccount, 5, 5, date);
+        cinema.buy(secondAccount, 5, 5, date);
+    }
 }
