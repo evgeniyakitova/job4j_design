68e17923df881271b31e65bd69f1ae88d09c2edf
evgeniyakitova <kitova.evgeniya.00@mail.ru>
2022-02-23 12:28:32 +0300

    1. Динамическое перераспределение продуктов [#854]

--- src/main/java/ru/job4j/lsp/ControlQuality.java
+++ src/main/java/ru/job4j/lsp/ControlQuality.java
@@ -1,5 +1,6 @@
 package ru.job4j.lsp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ControlQuality {
@@ -15,6 +16,17 @@ public class ControlQuality {
                 store.add(product);
                 break;
             }
+        }
+    }
+
+    public void resort() {
+        List<Food> products = new ArrayList<>();
+        for (Store store : storages) {
+            products.addAll(store.getAll());
+            store.clear();
+        }
+        for (Food product : products) {
+            addFood(product);
         }
     }
 }
